a1827b498295da823a0acb60bb71bacc14d7643a
hulkkk <wfoskk@gmail.com>
2022-05-02 11:42:01 +0900

    最低限の環境整備（amplify以外）

---
+++ src/component/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react'
+import css from './Layout.module.scss'
+
+/**
+ * タイトル：レイアウトコンポーネント
+ * 概要：全てのページに適用する画面構成スタイル用のコンポーネントである。
+ * @param {ReactNode} children 任意 初期値：なし
+ *
+ * @param
+ */
+
+type Props = {
+  children?: ReactNode
+}
+
+const Layout = (props: Props) => {
+  return (
+    <>
+      <div className={css.sample}>{props.children}</div>
+      <div>sample</div>
+    </>
+  )
+}
+
+export default Layout
